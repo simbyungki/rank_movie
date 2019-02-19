70498821fe2bbd0a0c080c20934a409c9247bb7e
simbyungki <dung8524@gmail.com>
2019-02-19 09:51:49 +0900

    Updates

---
+++ precache-manifest.e7e8d95786004708602837f58fd6268b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1183ada63ddd03b3d3ec",
+    "url": "/rank_movie/static/js/runtime~main.1183ada6.js"
+  },
+  {
+    "revision": "f81c0420d8a401d79bb9",
+    "url": "/rank_movie/static/js/main.f81c0420.chunk.js"
+  },
+  {
+    "revision": "f2fffbf57852fbf371ee",
+    "url": "/rank_movie/static/js/2.f2fffbf5.chunk.js"
+  },
+  {
+    "revision": "f81c0420d8a401d79bb9",
+    "url": "/rank_movie/static/css/main.3b38d8aa.chunk.css"
+  },
+  {
+    "revision": "40333a77ced6b8d81b4f48ee8dad7e31",
+    "url": "/rank_movie/index.html"
+  }
+];
